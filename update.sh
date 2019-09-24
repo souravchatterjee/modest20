5ff5f5e6acbd7d38102eeb66520a65a1b83a1e18
Sourav Chatterjee <sitari@gmail.com>
2019-09-24 20:23:01 +0530

    Added new information

--- update.sh
+++ update.sh
@@ -4,4 +4,4 @@ git add $1
 git commit -m \"$2\"
 git push
 
-rsync -Pavz $1 sourav.c@www.tifr.res.in:/home/depts/sourav.c/public_html/
+rsync -Pavz $1 modest20@www.tifr.res.in:/home/depts/modest20/public_html 
